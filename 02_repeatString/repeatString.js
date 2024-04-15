9b990ee4682ac3501fff594a5359d6fd04a61c74
Erick Maury <78674274+ErickMaury@users.noreply.github.com>
2024-04-15 19:43:17 -0400

    Passed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+	let input = string;
 
+	if (num < 0) {
+		return 'ERROR';
+	} else if (num === 0) {
+        return '';
+    }
+
+	for (let i = 1; i < num; i++) {
+		input += string;
+	}
+	return input;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
