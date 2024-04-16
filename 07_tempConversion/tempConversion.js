b59b1f86608275af30dcf7658b6106a01423d4ee
Erick Maury <78674274+ErickMaury@users.noreply.github.com>
2024-04-15 22:14:11 -0400

    Passed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (input) {
+	let celcius = 0.0;
+	celcius = (input - 32) * 0.5555;
+	return Number.parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (input) {
+  let fahrenheit = 0.0;
+  fahrenheit = (input * 1.8) + 32;
+  return Number.parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
