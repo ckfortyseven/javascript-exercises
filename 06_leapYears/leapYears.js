db003cd87643a5815ce1e256e9e5e6bef693dc9e
Erick Maury <78674274+ErickMaury@users.noreply.github.com>
2024-04-15 21:49:43 -0400

    Passed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function (potentialLeapYear) {
+	if (potentialLeapYear % 4 === 0 && (potentialLeapYear % 100 !== 0 || potentialLeapYear % 400 === 0)) {
+		return true;
+	} else return false;
 };
 
 // Do not edit below this line
